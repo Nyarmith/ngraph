fd3249d62204c8a7c3857aef5558df69b79d8d5b
nyarmith <powah.serge@gmail.com>
2017-12-03 18:30:29 -0500

    handler updated

--- include/handler.hpp
+++ include/handler.hpp
@@ -1,19 +1,24 @@
 #include "../util/util.hpp"
 #include "poller.hpp" //event
+#include <functional>
 #pragma once
 
 //callback class
 namespace ngl {
   class handler {
     public:
-      handler(int y, int x, int h, int w);
+      handler(int y, int x, int h, int w, std::function<void(event&)> f=[](event &e) {return ;});
+
       virtual bool intersect(int y, int x){
-          return (y_ <= y && y < y_+ h_ && x_ <= x && x < x_ + w_);
+        return (y_ <= y && y < y_+ h_ && x_ <= x && x < x_ + w_);
       }
 
-      virtual void handle(event e) = 0;
+      virtual void handle(event &e){
+        f_(e);
+      }
     private:
       int y_, x_;
       int h_, w_;
+      std::function<void(event &)> f_;
   };
 }
