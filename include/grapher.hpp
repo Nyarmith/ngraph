62e3b31975504a332a205baa4aff24b1a89f7118
nyarmith <powah.serge@gmail.com>
2017-12-03 20:14:45 -0500

    revised

--- include/grapher.hpp
+++ include/grapher.hpp
@@ -7,10 +7,11 @@
 class grapher{
   typedef enum {quitting, running, dragging} states;
   public:
-    grapher(WINDOW* in) : canv_(in){
+    grapher(WINDOW* in) : scene_(in){
       state_ = running;
       noecho();
       keypad(in, TRUE);
+      setup_program();
     }
     grapher& operator=(grapher other) = delete;
     ~grapher() = default;
@@ -25,18 +26,21 @@ class grapher{
     }
   private:
     states state_;
+
+    void setup_program(); //set up handlers and items and whatever
+
     void handle();  // well. this will be a lot of work
     
     void draw(){
       typedef std::vector<ngl::entity*>::iterator iter;
-      canv_.clear();
+      scene_.clear();
       for (iter i = ents_.begin(); i != ents_.end(); ++i)
-        (*i)->draw(canv_);
+        (*i)->draw(scene_);
       for (iter i = menu_.begin(); i != menu_.end(); ++i)
-        (*i)->draw(canv_);
-      canv_.refresh();
+        (*i)->draw(scene_);
+      scene_.refresh();
     }
-    ngl::canvas canv_;
+    ngl::window scene_;
     //all things being rendered are in here
     std::vector<ngl::entity*> ents_;
     std::vector<ngl::entity*> menu_;
@@ -44,35 +48,10 @@ class grapher{
 };
 
 
+//do stuff t update scene
 void grapher::handle(){
-  static int o_x=0;
-  static int o_y=0;
-  //check for events
-  int e = handler_.poll();
-  MEVENT* evnt=NULL;
-  if (e != -1){
-    if (ngl::handler::is_mouse(e))
-      evnt = ngl::handler::get_mouse_event();
-  }
-
-
-  //if we have a mouse event
-  if (evnt != NULL){
-    if (evnt->bstate & BUTTON1_PRESSED && state_ != dragging){
-      state_ = dragging;
-      o_x = evnt->x;
-      o_y = evnt->y;
-    }
-    else if (evnt->bstate & BUTTON1_PRESSED && state_ == dragging){
-      canv_.square(o_y, o_x, evnt->y, evnt->x);
-    }
-    else if (evnt->bstate & BUTTON1_RELEASED && state_ == dragging){
-      state_ = running;
-      ents_.push_back(new ngl::square(o_y, o_x, evnt->y, evnt->x));
-    }
-  }
-  else if (e != -1){
-    addch(e);
-  }
 }
 
+void grapher::setup_program(){
+  //do everything 
+}
