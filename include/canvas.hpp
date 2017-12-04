d2e14dd49da8a0e7c7776a0c913143bb8400084f
nyarmith <powah.serge@gmail.com>
2017-12-04 17:33:38 -0500

    cleaned

--- include/canvas.hpp
+++ include/canvas.hpp
@@ -1,6 +1,7 @@
 #include "../util/util.hpp"
 #pragma once
 
+//Drawing Interface
 namespace ngl {
     class canvas {
       public:
@@ -15,12 +16,3 @@ namespace ngl {
     };
 }
 
-
-
-//init_pair(1, COLOR_CYAN, COLOR_BLACK);
-//init_pair(2, COLOR_YELLOW, COLOR_MAGENTA);
-//init_pair(3, COLOR_YELLOW, COLOR_BLUE);
-//init_pair(4, COLOR_YELLOW, COLOR_RED);
-//init_pair(5, COLOR_BLACK, COLOR_WHITE);
-//init_pair(6, COLOR_BLACK, COLOR_YELLOW);
-//init_pair(7, COLOR_BLACK, COLOR_CYAN);
