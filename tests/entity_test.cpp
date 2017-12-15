0d6cc7df3366d04c5056bb62fe74ff4e0c7eb20c
nyarmith <powah.serge@gmail.com>
2017-12-14 23:46:23 -0500

    added alternate solution in comment

--- tests/entity_test.cpp
+++ tests/entity_test.cpp
@@ -36,7 +36,7 @@ int main(){
   main_window.add_entity(&r);
 
   // TODO: Figure out how to pass bound method
-  // main_window.add_handler(new ngl::handler(0,0,60,60,r.update));
+  // main_window.add_handler(new ngl::handler(0,0,60,60,r.update));  //could also make a lambda that runs r.update()
   std::function<void(const ngl::event &e)> g = std::bind(update, _1, r);
   main_window.add_handler(new ngl::handler(0,0,60,60,g));
 
