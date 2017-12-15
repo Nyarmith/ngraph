4e2c601444d071141f0d68b9e20172a42d402e51
nyarmith <powah.serge@gmail.com>
2017-12-15 08:48:49 -0500

    more test tinkering

--- tests/entity_test.cpp
+++ tests/entity_test.cpp
@@ -3,8 +3,8 @@
 #include "../include/cursapp.hpp"
 
 class mousefollower : public ngl::entity {
-  int y = 5; //C++11 wow
-  int x = 5;
+  int y;
+  int x;
   public:
   /* TODO: Figure out how I can pass this, see line 43
   void update(ngl::event e){
@@ -13,14 +13,15 @@ class mousefollower : public ngl::entity {
       x += y - e.y;
     }
   };*/
+  mousefollower(){ x = y = 5; }
   friend void update(const ngl::event &e, mousefollower &m);
   void draw(ngl::canvas& g){ g.square(y-1,x-1, 3, 3);}
 };
 
 void update(const ngl::event &e, mousefollower &m){
   if (e.type == ngl::EVENT::MOUSE){
-    m.x += (m.x - e.x)/4 ;
-    m.y += (m.y - e.y)/4 ;
+    m.x = e.x;
+    m.y = e.y;
   }
 };
 
@@ -37,7 +38,7 @@ int main(){
 
   // TODO: Figure out how to pass bound method
   // main_window.add_handler(new ngl::handler(0,0,60,60,r.update));  //could also make a lambda that runs r.update()
-  std::function<void(const ngl::event &e)> g = std::bind(update, _1, r);
+  std::function<void(const ngl::event &e)> g = std::bind(update, _1, r);  //test could be failing due to copy semantics here
   main_window.add_handler(new ngl::handler(0,0,60,60,g));
 
   m.addWindow(main_window);
