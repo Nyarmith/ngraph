f0d40f4535b6f1ef7a21e98f4f7238ee0ba07e28
nyarmith <powah.serge@gmail.com>
2018-01-18 23:00:26 -0500

    more test stuff

--- tests/gui_test3.cpp
+++ tests/gui_test3.cpp
@@ -4,8 +4,11 @@
 //make a form that takes a textbox and an attribute froma radial box and then applies that to the other screen(but several at a time)
 //also tests ability to pass rendering function and have that draw
 
-std::function<void(ngl::canvas &c)> drawer_factory(){
-  //TODO: return new drawing function that goes on the next line
+std::function<void(ngl::canvas &c)> drawer_factory(std::string s){
+  static int y = 1;
+  static int x = 1;
+  ++y;
+  return [s](ngl::canvas &c){ c.text(y,x,s.c_str());};
 }
 
 
@@ -17,10 +20,9 @@ int main(){
   ngl::window left  = windows[0];
   ngl::window right = windows[1];
 
-  left.add_entity(new boxform({"box","line","two boxes"}),
-      [](std::vector<std::string,std::string> &s){
-
-      });
+  left.add_entity(new ngl::textbox(0,0,30,30,
+        //callback that somehow creates and adds new entities to other window
+        ));
   right.add_entity();
   
   m.run();
