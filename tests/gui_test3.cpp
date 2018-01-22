e3b82633db7cf99332535b208db30ff7b3ce91db
nyarmith <powah.serge@gmail.com>
2018-01-22 18:18:25 -0500

    test fixed. static vars are not captured by value in a reference(I think they're the same as globals)

--- tests/gui_test3.cpp
+++ tests/gui_test3.cpp
@@ -6,10 +6,11 @@
 //this test currently works OK, but TODO: Make this test work fully as intended
 
 //the reason this doesn't work may be very deeply c++
-std::function<void(ngl::canvas&)> drawer_factory(std::string s){
-  static int y = 1;
-  static int x = 1;
-  ++y;
+std::function<void(ngl::canvas&)> drawer_factory(const std::string& s){
+  static int g_y=1; //fun fact: static variables aren't automatic in storage duration therefore aren't copied when captured in a lambda
+  const int y = g_y;
+  const int x = 1;
+  ++g_y;
   return [=](ngl::canvas &c){c.text(y,x,s.c_str());};
 }
 
@@ -25,7 +26,7 @@ int main(){
   left.add_entity(new ngl::textbox(0,0,5,20,
         "type a message to enter on the right",
         //callback that somehow creates and adds new entities to other window
-        [&right](std::string e){ right.add_entity(drawer_factory(e)); }
+        [&right](const std::string& e){ right.add_entity(drawer_factory(e)); }
         ));
 
   m.run();
