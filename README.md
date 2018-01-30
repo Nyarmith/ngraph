5dd95405386ee17e1f4579b3bc569965cde0ca5a
nyarmith <powah.serge@gmail.com>
2018-01-29 22:00:37 -0500

    v1.0, leaving alone for a month

--- README.md
+++ README.md
@@ -7,16 +7,15 @@
 4. .run() the cursapp class
 
 ### TODO:
-1. Add points, clearing, piece-preview, lose-state to tetris test-case. Invert colors.
-2. Make tests pass
-3. Create conan package
-4. Make the event-handler-in-separate-thread thing dima was talking about(it can refresh 30fps if it's a game)
-5. Make real-time mode work well over ssh tunnel
-6. Add optional global border[s] setting
-7. Give some way for the user to define their own custom color pairs https://stackoverflow.com/questions/33985896/how-to-enable-32k-color-pairs-in-ncurses
-8. Base functionality off of ncurses6 capabilities https://www.gnu.org/software/ncurses/
-9. Change y,x coord system to x,y(maybe)
-10. Function that accepts a window, makes a permanent border-window, and returns a new sub-window(or modifies the original window and keeps the border-window inaccessible)
-11. Spatial data structure for window entities, esp for default placement of entities
-12. Add "default or fixed selected window/entity" i.e. default or fixed global cursor positions
-13. Add font-control(if possible) to force the user's term to have a certain font
+1. Create conan package
+2. Make the event-handler-in-separate-thread thing dima was talking about(it can refresh 30fps if it's a game)
+3. Make real-time mode work well over ssh tunnel
+4. Add optional global border[s] setting
+5. Give some way for the user to define their own custom color pairs https://stackoverflow.com/questions/33985896/how-to-enable-32k-color-pairs-in-ncurses
+6. Base functionality off of ncurses6 capabilities https://www.gnu.org/software/ncurses/
+7. Change y,x coord system to x,y(maybe)
+8. Function that accepts a window, makes a permanent border-window, and returns a new sub-window(or modifies the original window and keeps the border-window inaccessible)
+9. Spatial data structure for window entities, esp for default placement of entities
+10. Add "default or fixed selected window/entity" i.e. default or fixed global cursor positions
+11. Add font-control(if possible) to force the user's term to have a certain font
+12. Condition variable + Mutex Condition Variable
--- tests/tetris.cpp
+++ tests/tetris.cpp
@@ -3,7 +3,9 @@
 
 const int b_hgt=22;
 const int b_wdt=10;
+int points=0;
 
+block* preview_block = NULL;
 block* cur_block = NULL;
 
 //top 2 rows should be hidden, int for each color_pair, colors:
@@ -12,7 +14,7 @@ int board[b_hgt][b_wdt] = {{0}};
 void draw_board(ngl::canvas &c){
   const int offset = 3;
   for (int col=0; col<8; ++col){
-    for (int x=0; x<b_wdt; ++x)
+    for (int x=0; x<b_wdt; ++x){
       for (int y=0; y<b_hgt; ++y){
         if (col == 0){
           c.add_char(offset + y,offset + x,'.' | COLOR_PAIR(col));
@@ -21,10 +23,74 @@ void draw_board(ngl::canvas &c){
           c.add_char(offset + y,offset + x,'#' | COLOR_PAIR(col));
         }
       }
+    }
   }
 }
 
+//right-window
+void piece_preview(ngl::canvas &c){
+  const int offset=4;
+  c.box(offset,offset,6,6);
+  for (unsigned i=0; i<preview_block->block_.size(); ++i){
+    for (unsigned j=0; j<preview_block->block_[i].size(); ++j){
+      if (preview_block->block_[i][j] != ' ')
+        c.add_char(offset+i+2, offset+j+1, preview_block->block_[i][j] |
+          COLOR_PAIR(preview_block->color_));
+    }
+  }
+}
 
+//left-window
+void point_score(ngl::canvas &c){
+  const int offset=4;
+  c.box (offset,offset, 3, 8);
+  c.set_color(2);
+  c.text(offset+1, offset+1,std::to_string(points));
+  c.set_color(2,false);
+}
+
+void tetris_check(){
+  int clears=0;
+  bool clear;
+  //check each row for bottom-up
+  for (int r=b_hgt-1; r>-0; --r){
+    clear=true;
+    for (int w=0; w < b_wdt; ++w){
+      //check each row, shift the tetorisu blocks down
+      clear = clear && (board[r][w] != 0);
+    }
+    if (clear){
+      clears++;
+      //move everything down
+      for (int rw = r; rw > 0; --rw){
+        for (int bw = 0; bw < b_wdt; ++bw){
+            board[rw][bw] = board[rw-1][bw];
+        }
+      }
+      for (int bw = 0; bw < b_wdt; ++bw){
+        board[0][bw] = 0;
+      }
+    }
+  }
+
+  switch(clears){
+    case 0:
+      points += 2;
+      break;
+    case 1:
+      points += 40;
+      break;
+    case 2:
+      points += 100;
+      break;
+    case 3:
+      points += 300;
+      break;
+    case 4:
+      points += 1200;
+      break;
+  }
+}
 
 bool collide(block* m){
   //x and y are top left
@@ -62,17 +128,13 @@ void draw_block(block* m){
 }
 
 void spawn(){
-  cur_block = generate_teromino();
+  cur_block = preview_block;
+  preview_block = generate_teromino();
   const int y=0, x=3;
   cur_block->y = y;
   cur_block->x = x;
 
   draw_block(cur_block);
-}
-
-
-void tetris_check(){
-  //check starting w/ the bottom rows for a tetris, then move up
 }
 
 void move_down(block* m){
@@ -159,7 +221,10 @@ void update_game(const ngl::event& s){
 int main(){
   ngl::cursapp &m = ngl::cursapp::instance();
   custom_colors();
+  preview_block = generate_teromino();
   auto wins = m.partition(3);
+  wins[0].add_entity(point_score);
   wins[1].add_entity(draw_board, update_game);
+  wins[2].add_entity(piece_preview);
   m.run();
 }
